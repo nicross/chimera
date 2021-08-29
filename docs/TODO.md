80000e528121a33c2a18ee5132301f71744e8d5a
nicross <nicross@users.noreply.github.com>
2021-08-28 19:06:01 -0500

    Add transients to wind

--- docs/TODO.md
+++ docs/TODO.md
@@ -1,7 +1,6 @@
 # TODO
 - Drone synth enhancements
 - Drone filter that colors each space
-- Foilage / transient noise
 - Submit to Rainbow Jam 2021 (possibly Brackeys Game Jam 2021.2)
 
 ## Nice seeds
--- src/js/content/audio/wind.js
+++ src/js/content/audio/wind.js
@@ -1,13 +1,41 @@
 content.audio.wind = (() => {
   const bus = content.audio.createBus()
 
+  const modDepthField = engine.utility.createNoiseWithOctaves({
+    octaves: 4,
+    seed: ['audio', 'wind', 'modDepth'],
+    type: engine.utility.simplex3d,
+  })
+
+  const modFrequencyField = engine.utility.createNoiseWithOctaves({
+    octaves: 4,
+    seed: ['audio', 'wind', 'modFrequency'],
+    type: engine.utility.simplex3d,
+  })
+
+  const physicalScale = 100 / engine.utility.simplex2d.prototype.skewFactor,
+    timeScale = 2 / engine.utility.simplex2d.prototype.skewFactor
+
   let binaural,
+    binauralSynth,
     synth
 
   bus.gain.value = engine.utility.fromDb(-9)
 
-  function calculateFrequency(strength) {
-    return engine.utility.lerpExp(20, 200, strength, 4)
+  function calculateHighFrequency(strength) {
+    return engine.utility.lerpExp(2000, 20000, strength, 0.25)
+  }
+
+  function calculateHighGain(strength) {
+    return engine.utility.fromDb(engine.utility.lerp(-34.5, -25.5, strength))
+  }
+
+  function calculateLowFrequency(strength) {
+    return engine.utility.lerpExp(20, 160, strength, 4)
+  }
+
+  function calculateQ(strength) {
+    return engine.utility.lerpExp(20, 0.001, strength)
   }
 
   function calculatePan(angle) {
@@ -25,26 +53,59 @@ content.audio.wind = (() => {
     const angle = content.wind.angle(position.x, position.y),
       strength = content.wind.strength(position.x, position.y)
 
-    synth = engine.audio.synth.createBuffer({
+    binauralSynth = engine.audio.synth.createBuffer({
       buffer: engine.audio.buffer.noise.brown(),
       gain: 1,
     }).filtered({
-      frequency: calculateFrequency(strength),
+      frequency: calculateLowFrequency(strength),
     })
 
     binaural = engine.audio.binaural.create(
       calculatePan(angle)
-    ).from(synth).to(bus)
+    ).from(binauralSynth).to(bus)
+
+    const modDepth = getModDepth()
+
+    synth = engine.audio.synth.createAmBuffer({
+      buffer: engine.audio.buffer.noise.white(),
+      carrierGain: 1 - modDepth,
+      gain: calculateHighGain(strength),
+      modDepth: modDepth,
+      modFrequency: getModFrequency(),
+    }).filtered({
+      frequency: calculateHighFrequency(strength),
+      Q: calculateQ(strength),
+    }).connect(bus)
   }
 
   function destroySynth() {
-    if (synth) {
-      synth.stop()
+    if (binauralSynth) {
+      binauralSynth.stop()
     }
 
     if (binaural) {
       binaural.destroy()
     }
+
+    if (synth) {
+      synth.stop()
+    }
+  }
+
+  function getModDepth() {
+    const {x, y} = engine.position.getVector()
+    const time = content.time.value()
+    const value = modDepthField.value(x / physicalScale, y / physicalScale, time / timeScale)
+
+    return engine.utility.lerpExp(0, 1/3, value, 2)
+  }
+
+  function getModFrequency() {
+    const {x, y} = engine.position.getVector()
+    const time = content.time.value()
+    const value = modFrequencyField.value(x / physicalScale, y / physicalScale, time / timeScale)
+
+    return engine.utility.lerpExp(engine.const.zeroGain, 8, value, 4)
   }
 
   function updateSynth() {
@@ -53,11 +114,18 @@ content.audio.wind = (() => {
     const angle = content.wind.angle(position.x, position.y),
       strength = content.wind.strength(position.x, position.y)
 
-    const frequency = calculateFrequency(strength),
+    const lowFrequency = calculateLowFrequency(strength),
+      modDepth = getModDepth(),
       pan = calculatePan(angle)
 
-    engine.audio.ramp.set(synth.filter.frequency, frequency)
+    engine.audio.ramp.set(binauralSynth.filter.frequency, lowFrequency)
     binaural.update(pan)
+
+    engine.audio.ramp.set(synth.filter.frequency, calculateHighFrequency(strength))
+    engine.audio.ramp.set(synth.filter.Q, calculateQ(strength))
+    engine.audio.ramp.set(synth.param.carrierGain, 1 - modDepth)
+    engine.audio.ramp.set(synth.param.gain, calculateHighGain(strength))
+    engine.audio.ramp.set(synth.param.mod.depth, modDepth)
   }
 
   return {
@@ -67,7 +135,7 @@ content.audio.wind = (() => {
       return this
     },
     update: function () {
-      if (!synth) {
+      if (!binauralSynth) {
         createSynth()
       }
 
