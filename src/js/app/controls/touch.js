c099f5aacdbc389a68a36875c5cc7108e0dc194c
nicross <nicross@users.noreply.github.com>
2021-08-29 02:06:25 -0500

    Add basic touch support

---
+++ src/js/app/controls/touch.js
@@ -0,0 +1,29 @@
+app.controls.mouse = (() => {
+  let touched = false
+
+  window.addEventListener('touchend', onTouchend)
+  window.addEventListener('touchstart', onTouchstart)
+
+  function onTouchend() {
+    touched = false
+  }
+
+  function onTouchstart() {
+    touched = true
+  }
+
+  return {
+    game: function () {
+      return {}
+    },
+    ui: function () {
+      const state = {}
+
+      if (touched) {
+        state.automove = true
+      }
+
+      return state
+    },
+  }
+})()
